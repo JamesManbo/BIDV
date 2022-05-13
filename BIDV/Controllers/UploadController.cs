8226a0e658b07e515f35c89d0e99ef027282b81e
JamesManbo <anhtuan02399@gmail.com>
2022-05-13 11:19:11 +0700

    Handel multiple upload images
    Handel multiple upload images

--- BIDV/Controllers/UploadController.cs
+++ BIDV/Controllers/UploadController.cs
@@ -41,7 +41,232 @@ namespace BIDV.Controllers
             }
             return Json(new { status = true, msg = "Upload thành công", files = lstFiles });
         }
+[AcceptVerbs("GET", "POST")]
+        public IHttpActionResult ImageUpload(int id) //Up ảnh cho khi thêm mới campaign
+        {
+            using (OnfluencerEntities db = new OnfluencerEntities())
+            {
+                var path = "";
+                string result = "";
+                try
+                {
+                    //for tệp ảnh mới
+                    for (int i = 0; i < HttpContext.Current.Request.Files.Count; i++)
+                    {
+                        var file = HttpContext.Current.Request.Files[i];
+                        var isAvatar = file.FileName.Split('.')[0] == "avatar";
+                        var filenameImage = DateTime.Now.Ticks + ".jpg";
+                        //check xem có thư mục imageCampaign chưa, chưa có thì tạo mới
+                        if (!Directory.Exists(HttpContext.Current.Server.MapPath("~/imageCampaign")))
+                        {
+                            Directory.CreateDirectory(HttpContext.Current.Server.MapPath("~/imageCampaign"));
+                        }
+                        Bitmap bmp = new Bitmap(HttpContext.Current.Request.Files[i].InputStream);
+                        OrientImage(bmp);
 
+                        // tạo chuối file path
+                        path = Path.Combine(HttpContext.Current.Server.MapPath("~/imageCampaign"), filenameImage);
+                        //file.SaveAs(path);
+                        path = path.ToLower();
+                        bmp.Save(path);
+                        var pathNewFile = path.Replace(".jpeg", "_m.jpg").Replace(".jpg", "_m.jpg").Replace(".png", "_m.jpg").Replace(".svg", "_m.jpg").Replace(".gif", "_m.jpg");
+
+                        using (Image image = Image.FromFile(path))
+                        {
+                            using (var resized = ImageUtilities.ScaleImage(image, 800, 800))
+                            {
+                                //save the resized image as a jpeg with a quality of 90
+                                ImageUtilities.SaveJpeg(pathNewFile, resized, 90);
+                            }
+                        }
+                        result = "/imageCampaign/" + filenameImage;
+                        var ImageUrlThumbnail = result.Replace(".jpg", "_m.jpg");
+
+                        CampaignImage objImage = new CampaignImage();
+                        objImage.CampaignId = id;
+                        objImage.ImageUrl = result;
+                        objImage.ImageUrlThumbnail = ImageUrlThumbnail;
+                        objImage.IsAvatar = isAvatar;
+                        db.CampaignImages.Add(objImage);
+                        //Add db table ImageCampaign
+
+                    }
+                    // for tệp ảnh cũ
+                    foreach (var key in HttpContext.Current.Request.Form.AllKeys)
+                    {
+                        var isAvatar = key == "avatar";
+                        var imgId = int.Parse(HttpContext.Current.Request.Form[key]);
+                        var objImg = db.CampaignImages.FirstOrDefault(t => t.Id == imgId);
+                        if (objImg != null)
+                        {
+                            CampaignImage objImage = new CampaignImage();
+                            objImage.CampaignId = id;
+                            objImage.ImageUrl = objImg.ImageUrl;
+                            objImage.ImageUrlThumbnail = objImg.ImageUrlThumbnail;
+                            objImage.IsAvatar = isAvatar;
+                            db.CampaignImages.Add(objImage);
+                        }
+                    }
+                    db.SaveChanges();
+                    //for (int i = 0; i < HttpContext.Current.Request.Form.Count; i++)
+                    //{
+                    //    var file = HttpContext.Current.Request.Form[i];
+                    //    var debug = file.ToString();
+                    //var isAvatar = file..FileName.Split('.')[0] == "avatar";
+                    //var filenameImage = file.FileName.Split('.')[0] + DateTime.Now.ToString("yyyyMMddTHHmmss") + ".jpg";
+                    //DateTime time = DateTime.Now;
+                    ////check xem có thư mục imageCampaign chưa, chưa có thì tạo mới
+                    //if (!Directory.Exists(HttpContext.Current.Server.MapPath("~/imageCampaign")))
+                    //{
+                    //    Directory.CreateDirectory(HttpContext.Current.Server.MapPath("~/imageCampaign"));
+                    //}
+                    //// tạo chuối file path
+                    //path = Path.Combine(HttpContext.Current.Server.MapPath("~/imageCampaign"), filenameImage);
+                    //file.SaveAs(path);
+                    //result = "/imageCampaign/" + filenameImage;
+                    ////Add db table ImageCampaign
+                    //CampaignImage objImage = new CampaignImage();
+                    //objImage.CampaignId = id;
+                    //objImage.ImageUrl = result;
+                    //objImage.IsAvatar = isAvatar;
+                    //db.CampaignImages.Add(objImage);
+                    //db.SaveChanges();
+                    //}
+                    return ResponseMessage(Request.CreateResponse(HttpStatusCode.OK, result));
+                }
+                catch (Exception ex)
+                {
+                    result = ex.ToString();
+                    return ResponseMessage(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, result));
+                }
+            }
+        }
+
+        [AcceptVerbs("GET", "POST")]
+        public IHttpActionResult ImageUploadForEdit(int id) //Up ảnh cho campaign
+        {
+            using (OnfluencerEntities db = new OnfluencerEntities())
+            {
+                var path = "";
+                string result = "";
+                try
+                {
+                    // xóa ảnh cũ không sử dụng
+                    var lstImageId = new List<int>();
+                    foreach (var key in HttpContext.Current.Request.Form.AllKeys)
+                    {
+                        var isAvatar = key == "avatar";
+                        var imgId = int.Parse(HttpContext.Current.Request.Form[key]);
+                        CampaignImage objImg = db.CampaignImages.Find(imgId);
+                        objImg.IsAvatar = isAvatar;
+                        lstImageId.Add(imgId);
+                    }
+
+                    var lstCampaignImgRemove = db.CampaignImages.Where(t => t.CampaignId == id && !lstImageId.Contains(t.Id)).ToList();
+                    foreach (var objRemove in lstCampaignImgRemove)
+                    {
+                        db.CampaignImages.Remove(objRemove);
+                    }
+
+                    //for tệp ảnh mới
+                    for (int i = 0; i < HttpContext.Current.Request.Files.Count; i++)
+                    {
+                        var file = HttpContext.Current.Request.Files[i];
+                        var isAvatar = file.FileName.Split('.')[0] == "avatar";
+                        var filenameImage = DateTime.Now.Ticks + ".jpg";
+                        DateTime time = DateTime.Now;
+                        //check xem có thư mục imageCampaign chưa, chưa có thì tạo mới
+                        if (!Directory.Exists(HttpContext.Current.Server.MapPath("~/imageCampaign")))
+                        {
+                            Directory.CreateDirectory(HttpContext.Current.Server.MapPath("~/imageCampaign"));
+                        }
+
+                        Bitmap bmp = new Bitmap(HttpContext.Current.Request.Files[i].InputStream);
+                        OrientImage(bmp);
+
+                        // tạo chuối file path
+                        path = Path.Combine(HttpContext.Current.Server.MapPath("~/imageCampaign"), filenameImage);
+                        //file.SaveAs(path);
+                        path = path.ToLower();
+                        bmp.Save(path);
+                        var pathNewFile = path.Replace(".jpeg", "_m.jpg").Replace(".jpg", "_m.jpg").Replace(".png", "_m.jpg").Replace(".svg", "_m.jpg").Replace(".gif", "_m.jpg");
+
+                        // tạo chuối file path
+                        //path = Path.Combine(HttpContext.Current.Server.MapPath("~/imageCampaign"), filenameImage);
+                        //file.SaveAs(path);
+                        //var pathNewFile = path.Replace(".jpg", "_m.jpg").Replace(".png", "_m.jpg");
+
+                        using (Image image = Image.FromFile(path))
+                        {
+                            using (var resized = ImageUtilities.ScaleImage(image, 800, 800))
+                            {
+                                //save the resized image as a jpeg with a quality of 90
+                                ImageUtilities.SaveJpeg(pathNewFile, resized, 90);
+                            }
+                        }
+                        result = "/imageCampaign/" + filenameImage;
+                        var ImageUrlThumbnail = result.Replace(".jpg", "_m.jpg");
+                        //Add db table ImageCampaign
+                        CampaignImage objImage = new CampaignImage();
+                        objImage.CampaignId = id;
+                        objImage.ImageUrl = result;
+                        objImage.ImageUrlThumbnail = ImageUrlThumbnail;
+                        objImage.IsAvatar = isAvatar;
+                        db.CampaignImages.Add(objImage);
+                    }
+                    db.SaveChanges();
+
+
+                    //foreach (var key in HttpContext.Current.Request.Form.AllKeys)
+                    //{
+                    //    var isAvatar = key == "avatar";
+                    //    var imgId = int.Parse(HttpContext.Current.Request.Form[key]);
+                    //    var objImg = db.CampaignImages.FirstOrDefault(t => t.Id == imgId);
+                    //    if (objImg != null)
+                    //    {
+                    //        CampaignImage objImage = new CampaignImage();
+                    //        objImage.CampaignId = id;
+                    //        objImage.ImageUrl = objImg.ImageUrl;
+                    //        objImage.IsAvatar = isAvatar;
+                    //        db.CampaignImages.Add(objImage);
+                    //        db.SaveChanges();
+                    //    }
+
+                    //}
+
+                    //for (int i = 0; i < HttpContext.Current.Request.Form.Count; i++)
+                    //{
+                    //    var file = HttpContext.Current.Request.Form[i];
+                    //    var debug = file.ToString();
+                    //var isAvatar = file..FileName.Split('.')[0] == "avatar";
+                    //var filenameImage = file.FileName.Split('.')[0] + DateTime.Now.ToString("yyyyMMddTHHmmss") + ".jpg";
+                    //DateTime time = DateTime.Now;
+                    ////check xem có thư mục imageCampaign chưa, chưa có thì tạo mới
+                    //if (!Directory.Exists(HttpContext.Current.Server.MapPath("~/imageCampaign")))
+                    //{
+                    //    Directory.CreateDirectory(HttpContext.Current.Server.MapPath("~/imageCampaign"));
+                    //}
+                    //// tạo chuối file path
+                    //path = Path.Combine(HttpContext.Current.Server.MapPath("~/imageCampaign"), filenameImage);
+                    //file.SaveAs(path);
+                    //result = "/imageCampaign/" + filenameImage;
+                    ////Add db table ImageCampaign
+                    //CampaignImage objImage = new CampaignImage();
+                    //objImage.CampaignId = id;
+                    //objImage.ImageUrl = result;
+                    //objImage.IsAvatar = isAvatar;
+                    //db.CampaignImages.Add(objImage);
+                    //db.SaveChanges();
+                    //}
+                    return ResponseMessage(Request.CreateResponse(HttpStatusCode.OK, result));
+                }
+                catch (Exception ex)
+                {
+                    result = ex.ToString();
+                    return ResponseMessage(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, result));
+                }
+            }
+        }
         
     }
 }
